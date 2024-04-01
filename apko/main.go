e028acdfcae5a531faed202bf6e38999f2697e06
purpleclay <purpleclaygh@gmail.com>
2024-04-01 07:43:54 +0100

    feat: support overriding options to the apko build command

--- apko/main.go
+++ apko/main.go
@@ -2,7 +2,9 @@
 package main
 
 import (
+	"context"
 	"dagger/apko/internal/dagger"
+	"fmt"
 	"strings"
 
 	"chainguard.dev/apko/pkg/build/types"
@@ -28,7 +30,36 @@ func (a *Apko) Load(
 
 // Generates and loads a pre-configured apko configuration file for
 // building an image based on the Wolfi OS
-func (a *Apko) WithWolfi() (*ApkoConfig, error) {
+//
+// Examples:
+//
+// # Generate a default Wolfi OS apko configuration file
+// $ dagger call with-wolfi --entrypoint="/bin/sh -l"
+//
+// # Extend the default Wolfi OS apko configuration file
+// $ dagger call with-wolfi --entrypoint="echo \$VAR1" --env="VAR1:VALUE1"
+func (a *Apko) WithWolfi(
+	// the command to execute after the container entrypoint
+	// +optional
+	cmd string,
+	// the entrypoint to the container
+	// +required
+	entrypoint string,
+	// a list of environment variables to set within the container image, expected in (key:value) format
+	// +optional
+	env []string,
+) (*ApkoConfig, error) {
+	environment := map[string]string{}
+	if len(env) > 0 {
+		for _, e := range env {
+			key, value, found := strings.Cut(e, ":")
+			if !found {
+				return nil, fmt.Errorf("failed to parse malformed environment variable argument", e)
+			}
+			environment[strings.TrimSpace(key)] = strings.TrimSpace(value)
+		}
+	}
+
 	cfg := types.ImageConfiguration{
 		Contents: types.ImageContents{
 			Repositories: []string{"https://packages.wolfi.dev/os"},
@@ -39,11 +70,14 @@ func (a *Apko) WithWolfi() (*ApkoConfig, error) {
 			},
 		},
 		Entrypoint: types.ImageEntrypoint{
-			Command: "/bin/sh -l",
+			Type:    "service-bundle",
+			Command: entrypoint,
 		},
+		Cmd: cmd,
 		Archs: []types.Architecture{
 			types.ParseArchitecture("x86_64"),
 		},
+		Environment: environment,
 	}
 
 	out, err := yaml.Marshal(&cfg)
@@ -51,13 +85,17 @@ func (a *Apko) WithWolfi() (*ApkoConfig, error) {
 		return nil, err
 	}
 
-	// TODO: change Cfg to be a *Directory
 	dir := dag.Directory().
 		WithNewFile("apko.yaml", string(out), dagger.DirectoryWithNewFileOpts{Permissions: 0o644})
 
 	return &ApkoConfig{
 		Cfg: dir.File("apko.yaml"),
 	}, nil
+}
+
+// Prints the generated apko configuration file to stdout
+func (a *ApkoConfig) Yaml(ctx context.Context) (string, error) {
+	return a.Cfg.Contents(ctx)
 }
 
 // Builds an image from an apko configuration file and outputs it as a file
@@ -67,11 +105,36 @@ func (a *Apko) WithWolfi() (*ApkoConfig, error) {
 //
 // Examples:
 //
-// dagger call load --cfg apko.yaml build --ref registry:5000/example:latest
+// # Build an OCI image from a provided apko configuration file
+// $ dagger call load --cfg apko.yaml build --ref registry:5000/example:latest
+//
+// # Build an OCI image based on the Wolfi OS
+// $ dagger call with-wolfi build --ref registry:5000/example:latest
 func (a *ApkoConfig) Build(
+	// additional OCI annotations to add to the built image, expected in (key:value) format
+	// +optional
+	annotations []string,
+	// a list of architectures to build, overwriting the config
+	// +optional
+	archs []string,
+	// a list of additional packages to include within the built image
+	// +optional
+	pkgs []string,
+	// a list of additional repositories used to pull packages into the built image
+	// +optional
+	repos []string,
 	// the image reference to build
 	// +required
-	ref string) *File {
+	ref string,
+	// detect and embed VCS URLs within the built OCI image
+	// +optional
+	// default=true
+	vcs bool,
+	// generate and embed an SBOM (software bill of materials) within the built OCI image
+	// +optional
+	// +default=true
+	sbom bool,
+) *Directory {
 	image := ref
 	if pos := strings.LastIndex(image, "/"); pos > -1 {
 		image = image[pos+1:]
@@ -82,9 +145,41 @@ func (a *ApkoConfig) Build(
 	}
 	image = image + ".tar"
 
+	cmd := []string{
+		"build",
+		"/apko/apko.yaml",
+		ref,
+		image,
+	}
+
+	if len(archs) > 0 {
+		cmd = append(cmd, "--arch", strings.Join(archs, ","))
+	}
+
+	if len(repos) > 0 {
+		cmd = append(cmd, "--repository-append", strings.Join(repos, ","))
+	}
+
+	if len(pkgs) > 0 {
+		cmd = append(cmd, "--package-append", strings.Join(pkgs, ","))
+	}
+
+	if len(annotations) > 0 {
+		cmd = append(cmd, "--annotations", strings.Join(annotations, ","))
+	}
+
+	if !sbom {
+		cmd = append(cmd, "--sbom=false")
+	}
+
+	if !vcs {
+		cmd = append(cmd, "--vcs=false")
+	}
+
 	return dag.Container().
-		WithMountedFile("apko.yaml", a.Cfg).
 		From("cgr.dev/chainguard/apko").
-		WithExec([]string{"build", "apko.yaml", ref, image}).
-		File(image)
+		WithWorkdir("apko").
+		WithFile("apko.yaml", a.Cfg).
+		WithExec(cmd).
+		Directory("")
 }
