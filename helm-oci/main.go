e5ce3c13633696dcd7b0847636967c488c8f6db5
purpleclay <purpleclaygh@gmail.com>
2024-05-06 05:38:26 +0100

    feat: helm oci dagger module

---
+++ helm-oci/main.go
@@ -0,0 +1,121 @@
+// A lightweight wrapper around Helm OCI.
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+)
+
+const (
+	HelmGithubRepo = "helm/helm"
+	HelmBaseImage  = "alpine/helm"
+)
+
+// Helm OCI dagger module
+type HelmOci struct {
+	// +private
+	Base *Container
+}
+
+// Initializes the Helm OCI dagger module
+func New(
+	ctx context.Context,
+	// a custom base image containing an installation of helm
+	// +optional
+	base *Container,
+) (*HelmOci, error) {
+	var err error
+	if base == nil {
+		base, err = defaultImage(ctx)
+	} else {
+		if _, err = base.WithExec([]string{"version"}).Sync(ctx); err != nil {
+			return nil, err
+		}
+	}
+
+	return &HelmOci{Base: base}, err
+}
+
+func defaultImage(ctx context.Context) (*Container, error) {
+	tag, err := dag.Github().GetLatestRelease(HelmGithubRepo).Tag(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return dag.Container().
+		From(fmt.Sprintf("%s:%s", HelmBaseImage, tag[1:])), nil
+}
+
+// Packages a Helm chart and publishes it to an OCI registry
+func (m *HelmOci) PackagePush(
+	ctx context.Context,
+	// a path to the directory containing the Chart.yaml file
+	// +required
+	chart *Directory,
+	// the OCI registry to publish the chart to, should include full path without chart name
+	// +required
+	registry string,
+	// the username for authenticating with the registry
+	// +required
+	username string,
+	// the password for authenticating with the registry
+	// +required
+	password *Secret,
+) (string, error) {
+
+	ctr := m.Base.
+		WithMountedDirectory("/work", chart).
+		WithWorkdir("/work")
+
+	tgz, err := ctr.WithExec([]string{"package", "."}).Stdout(ctx)
+	if err != nil {
+		return "", err
+	}
+	tgz = tgz[strings.LastIndex(tgz, "/"):]
+
+	// Extract the registry host needed for logging in
+	idx := strings.Index(registry, "/")
+	if idx == -1 {
+		return "", fmt.Errorf("")
+	}
+	registryHost := registry[:idx]
+
+	return ctr.
+		WithEnvVariable("REGISTRY", registryHost).
+		WithEnvVariable("REGISTRY_USER", username).
+		WithSecretVariable("REGISTRY_PASSWORD", password).
+		WithExec([]string{"sh", "-c", "helm", "registry", "login", "$REGISTRY", "-u", "$REGISTRY_USER", "-p", "$REGISTRY_PASSWORD"}).
+		WithExec([]string{"push", tgz, fmt.Sprintf("oci://%s", registry)}).
+		Stdout(ctx)
+}
+
+// Lints a Helm chart
+func (m *HelmOci) Lint(
+	ctx context.Context,
+	// a path to the directory containing the Chart.yaml file
+	// +required
+	chart *Directory,
+	// fail on any linting errors by returning a non zero exit code
+	// +optional
+	strict bool,
+	// print only warnings and errors
+	// +optional
+	quiet bool) (string, error) {
+
+	cmd := []string{"lint", "."}
+
+	if strict {
+		cmd = append(cmd, "--strict")
+	}
+
+	if quiet {
+		cmd = append(cmd, "--quiet")
+	}
+
+	return m.Base.
+		WithMountedDirectory("/work", chart).
+		WithWorkdir("/work").
+		WithExec(cmd).
+		Stdout(ctx)
+}
