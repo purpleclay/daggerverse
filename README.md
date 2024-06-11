84ea03f63438eacf4e3296cdcef05d24c2c03218
purpleclay <purpleclaygh@gmail.com>
2024-06-11 06:35:31 +0100

    feat: add new shellcheck dagger function

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Welcome to the Daggerverse! Browse the collection of Dagger functions on offer, 
 - [OCI Login](https://daggerverse.dev/mod/github.com/purpleclay/daggerverse/oci-login): Create an OCI registry authentication file for secure authentication
 - [Ponysay](https://daggerverse.dev/mod/github.com/purpleclay/daggerverse/ponysay): Let's get a Pony to say something fun.
 - [Trivy](https://daggerverse.dev/mod/github.com/purpleclay/daggerverse/trivy): Scan container images and the filesystem for vulnerabilities.
+- [ShellCheck](https://daggerverse.dev/mod/github.com/purpleclay/daggerverse/shellcheck): A static analysis tool for your shell scripts
---
+++ shellcheck/main.go
@@ -0,0 +1,104 @@
+// ShellCheck, a static analysis tool for your shell scripts
+//
+// The goals of ShellCheck are:
+//
+// - To point out and clarify typical beginner's syntax issues that cause a shell to
+// give cryptic error messages.
+//
+// - To point out and clarify typical intermediate level semantic problems that cause a
+// shell to behave strangely and counter-intuitively.
+//
+// -To point out subtle caveats, corner cases and pitfalls that may cause an advanced user's
+// otherwise working script to fail under future circumstances.
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+const (
+	ShellcheckGithubRepo = "koalaman/shellcheck"
+	ShellcheckBaseImage  = "koalaman/shellcheck"
+	WorkingDir           = "/work"
+)
+
+// ShellCheck dagger module
+type Shellcheck struct {
+	// a custom base image containing an installation of shellcheck
+	// +private
+	Base *Container
+}
+
+// Initializes the ShellCheck dagger module
+func New(
+	ctx context.Context,
+	// a custom base image containing an installation of shellcheck
+	// +optional
+	base *Container,
+) (*Shellcheck, error) {
+	var err error
+	if base == nil {
+		base, err = defaultImage(ctx)
+	} else {
+		if _, err = base.WithExec([]string{"--version"}).Sync(ctx); err != nil {
+			return nil, err
+		}
+	}
+
+	return &Shellcheck{Base: base}, nil
+}
+
+func defaultImage(ctx context.Context) (*Container, error) {
+	tag, err := dag.Github().GetLatestRelease(ShellcheckGithubRepo).Tag(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return dag.Container().
+		From(fmt.Sprintf("%s:%s", ShellcheckBaseImage, tag)), nil
+}
+
+// Checks shell scripts for syntactic and semantic issues that may otherwise be difficult
+// to identify
+func (m *Shellcheck) Check(
+	ctx context.Context,
+	// the output format of the shellcheck report
+	// (checkstyle, diff, gcc, json, json1, quiet, tty)
+	// +optional
+	format string,
+	// a list of paths for checking
+	// +required
+	paths []string,
+	// the minimum severity of errors to consider when checking scripts
+	// (error, warning, info, style)
+	// +optional
+	severity string,
+	// the type of shell dialect to check against (sh, bash, dash, ksh, busybox)
+	// +optional
+	shell string,
+	// a path to a directory containing scripts to scan, this can be a project root
+	// +required
+	src *Directory,
+) (string, error) {
+	cmd := []string{}
+	if format != "" {
+		cmd = append(cmd, "--format", format)
+	}
+
+	if severity != "" {
+		cmd = append(cmd, "--severity", severity)
+	}
+
+	if shell != "" {
+		cmd = append(cmd, "--shell", shell)
+	}
+
+	cmd = append(cmd, paths...)
+
+	return m.Base.
+		WithDirectory(WorkingDir, src).
+		WithWorkdir(WorkingDir).
+		WithExec(cmd).
+		Stdout(ctx)
+}
