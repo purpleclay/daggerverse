9c4f2bbbfa3e087d11f32ff40c38b6f2e3896f10
purpleclay <purpleclaygh@gmail.com>
2024-10-19 08:05:02 +0100

    feat: add support for loading an existing netrc configuration file

--- netrc/main.go
+++ netrc/main.go
@@ -15,6 +15,15 @@ import (
 	"encoding/hex"
 	"fmt"
 	"strings"
+	"unicode"
+
+	"github.com/purpleclay/chomp"
+)
+
+const (
+	machineIdent  = "machine"
+	loginIdent    = "login"
+	passwordIdent = "password"
 )
 
 // Holds configuration details for logging into remote sites from a machine
@@ -87,6 +96,81 @@ func (m *Netrc) WithLogin(
 
 	m.Config.Logins = append(m.Config.Logins, login)
 	return m, nil
+}
+
+// Loads an existing auto-login configuration from a file. Can be chained to load multiple
+// configuration files in a single pass
+func (m *Netrc) WithFile(
+	ctx context.Context,
+	// an existing auto-login configuration file
+	// +required
+	cfg *dagger.File,
+) (*Netrc, error) {
+	config, err := cfg.Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	logins, err := fromConfiguration(config)
+	if err != nil {
+		return nil, err
+	}
+
+	m.Config.Logins = append(m.Config.Logins, logins...)
+	return m, nil
+}
+
+func fromConfiguration(cfg string) ([]Login, error) {
+	_, ext, err := chomp.Map(
+		chomp.ManyN(
+			chomp.All(
+				eatIdent(machineIdent),
+				eatIdent(loginIdent),
+				eatIdent(passwordIdent),
+			), 1),
+		func(in []string) []Login {
+			// comes in a series of three: (machine, login, password)
+			var logins []Login
+			for i := 0; i < len(in); i += 3 {
+				logins = append(logins, Login{
+					Machine:  in[i],
+					Username: in[i+1],
+					Password: in[i+2],
+				})
+			}
+			return logins
+		})(cfg)
+
+	return ext, err
+}
+
+type isWhitespace struct{}
+
+func (isWhitespace) Match(r rune) bool {
+	return unicode.IsSpace(r)
+}
+
+func (isWhitespace) String() string {
+	return "is_whitespace"
+}
+
+var IsWhitespace = isWhitespace{}
+
+func eatIdent(ident string) chomp.Combinator[string] {
+	return func(s string) (string, string, error) {
+		rem, ext, err := chomp.All(
+			chomp.Tag(ident),
+			chomp.While(IsWhitespace),
+			chomp.Not("\r\n "),
+			chomp.Opt(chomp.While(IsWhitespace)),
+			chomp.Opt(chomp.Crlf()),
+		)(s)
+		if err != nil {
+			return rem, "", err
+		}
+
+		return rem, ext[2], nil
+	}
 }
 
 // Generates and returns a .netrc file based on the current configuration
